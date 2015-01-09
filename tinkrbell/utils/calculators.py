5d88354357c03092700d45b601bb0dc3fddf880c
Allan Lei <allanlei@helveticode.com>
2015-01-09 18:56:22 +0800

    native python resize calculation

--- tinkrbell/utils/calculators.py
+++ tinkrbell/utils/calculators.py
@@ -2,22 +2,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-# def boundingbox(image, (bbw, bbh)):
-#     if max((width - bbw), (height - bbh)) > 0:
-#         if (width - bbw) > (height - bbh):
-#             resized_width = bbw
-#             resized_height = height / (width/resized_width)
-#         else:
-#             resized_height = bbh
-#             resized_width = width / (height/resized_height)
-#     else:
-#         resized_width, resized_height = width, height
-#     return int(resized_width), int(resized_height)
+def percentage(image, percent):
+    return int(round(image.width * percent)), int(round(image.height * percent))
 
 
 def boundingbox(image, (width, height)):
-    # TODO: This is just a shortcut to calculate resize
-    # NOTE: image.transform does not preserve animation
-    with image.clone() as img:
-        img.transform(resize='{:d}x{:d}>'.format(width, height))
-        return img.width, img.height
+    reduction = max(
+        (image.width - width) / image.width,
+        (image.height - height) / image.height,
+    )
+    return percentage(image, 1 - reduction)
